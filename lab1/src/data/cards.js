fd5493a2eb687e0f2020ba8ce2c7e1ebdb585398
daniel <145810043+dan0407@users.noreply.github.com>
2025-02-26 18:44:08 -0500

    Add files via upload

--- lab1/src/data/cards.js
+++ lab1/src/data/cards.js
@@ -1,22 +1,37 @@
 export const cardsData = [
-  {
-    title: "iPhone 15 Pro",
-    description: "The most powerful iPhone ever with A17 Pro chip",
-    image: "https://co.tiendasishop.com/cdn/shop/files/IMG-10935121_7a8938c0-9b21-40a0-bc7c-0bbb50567557.jpg?v=1723513524&width=823"
-  },
-  {
-    title: "MacBook Air M2",
-    description: "Incredibly thin and light. Supercharged by M2",
-    image: "https://camerastoreke.co.ke/wp-content/uploads/2025/02/Apple-13.6-inches-MacBook-Air-M2-Space-Gray-camerasafrica.jpg"
-  },
-  {
-    title: "iPad Pro",
-    description: "Supercharged by M2 chip. Brilliant Liquid Retina XDR display",
-    image: "https://cdnx.jumpseller.com/tiquemobile/image/35509650/resize/610/610?1684755346"
-  },
-  {
-    title: "Apple Watch Series 9",
-    description: "Your essential health companion with advanced features",
-    image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/refurb-45-stainless-graphite-sport-band-midnight-s9?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1709329400754"
-  }
-]
+	{
+		title: 'iPhone 15 Pro',
+		description: 'The most powerful iPhone ever with A17 Pro chip',
+		image:
+			'https://co.tiendasishop.com/cdn/shop/files/IMG-10935121_7a8938c0-9b21-40a0-bc7c-0bbb50567557.jpg?v=1723513524&width=823',
+	},
+	{
+		title: 'MacBook Air M2',
+		description: 'Incredibly thin and light. Supercharged by M2',
+		image:
+			'https://camerastoreke.co.ke/wp-content/uploads/2025/02/Apple-13.6-inches-MacBook-Air-M2-Space-Gray-camerasafrica.jpg',
+	},
+	{
+		title: 'iPad Pro',
+		description: 'Supercharged by M2 chip. Brilliant Liquid Retina XDR display',
+		image: 'https://cdnx.jumpseller.com/tiquemobile/image/35509650/resize/610/610?1684755346',
+	},
+	{
+		title: 'Apple Watch Series 9',
+		description: 'Your essential health companion with advanced features',
+		image:
+			'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/refurb-45-stainless-graphite-sport-band-midnight-s9?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1709329400754',
+	},
+	{
+		title: 'Airpods pro 2',
+		description: 'cuenta con el modo ambiente el cual te permitrá escuchar el mundo que te rodea. ',
+		image:
+			'https://www.alkosto.com/medias/195949052484-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w1NTY2fGltYWdlL3dlYnB8YUdJNUwyZzJOUzh4TkRnME1UYzNNREkzT0RrME1pOHhPVFU1TkRrd05USTBPRFJmTURBeFh6YzFNRmQ0TnpVd1NBfDE1NGVlNTlmNGZhNjA2NjEwM2I0NTI5MTY2ZWU2N2E1M2JkMzBjNDM2ZTBjOWMyNTA0Y2I4ZDU2MWExMzZjZjU',
+	},
+	{
+		title: 'Apple tv+',
+		description: 'Una gran experiencia de TV que dará mucho de qué hablar.',
+		image:
+			'https://i.ebayimg.com/images/g/2cAAAOSw~bxj0Fda/s-l400.png',
+	},
+];
